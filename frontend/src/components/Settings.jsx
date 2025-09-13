175dd8b2aacfbb99ab9aeb823ef72d516a334939
leketech <aaduraleke@aol.com>
2025-09-13 14:43:42 +0100

    Build Error Fixed

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { useTheme } from '../contexts/ThemeContext';
 import { Link, useNavigate } from 'react-router-dom';
 import api from '../utils/auth';
